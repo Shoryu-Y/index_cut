5222f034aa11d9fb96b073fd675f5c7061b5dc4c
Shoryu Yamamoto <tatsubee@pixiv.co.jp>
2024-02-24 16:31:16 +0900

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,21 +5,8 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int currentIndex = 0;
-  final items = [
-    const Text('1', style: TextStyle(fontSize: 18)),
-    const Text('2', style: TextStyle(fontSize: 18)),
-    const Text('3', style: TextStyle(fontSize: 18)),
-    const Text('4', style: TextStyle(fontSize: 18)),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -29,22 +16,45 @@ class _MyAppState extends State<MyApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(
+      home: const Scaffold(
         body: SafeArea(
-          child: IndexCut(
-            currentIndex: currentIndex,
-            icons: items,
-            isOneWay: false,
-            onChanged: (index) {
-              setState(() {
-                currentIndex = index;
-              });
-            },
-            child: Center(
-              child: items[currentIndex],
-            ),
-          ),
+          child: IndexCutView(),
         ),
+      ),
+    );
+  }
+}
+
+class IndexCutView extends StatefulWidget {
+  const IndexCutView({super.key});
+
+  @override
+  State<IndexCutView> createState() => _IndexCutViewState();
+}
+
+class _IndexCutViewState extends State<IndexCutView> {
+  int currentIndex = 0;
+
+  final items = [
+    const Text('1', style: TextStyle(fontSize: 18)),
+    const Text('2', style: TextStyle(fontSize: 18)),
+    const Text('3', style: TextStyle(fontSize: 18)),
+    const Text('4', style: TextStyle(fontSize: 18)),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return IndexCut(
+      currentIndex: currentIndex,
+      icons: items,
+      isOneWay: false,
+      onChanged: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      child: Center(
+        child: items[currentIndex],
       ),
     );
   }
